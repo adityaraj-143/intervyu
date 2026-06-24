92c8f44cc4dd7a20e6a433812fd7b8e99ac9b554
adityaraj-143 <adityaxraj72004@gmail.com>
2026-06-24 19:09:03 +0530

    feat: add GitHub scraper and wire up interview route

--- backend/routes/interview.routes.ts
+++ backend/routes/interview.routes.ts
@@ -1,14 +1,23 @@
+import axios from "axios";
 import type { Request, Response } from "express";
+import { githubScraper } from "../scrapers/githubscraper";
 
-export function handleInterviewStart(req: Request, res: Response): void {
+export async function handleInterviewStart(req: Request, res: Response): Promise<void> {
   const { githubUsername } = req.body;
 
   //TODO: webscrape the user's profile and repos
 
   if (!githubUsername) {
-    res.status(400).json({ error: "GitHub username is required" });
-    return;
+   res.status(400).json({ error: "GitHub username is required" });
   }
+
+  const resp = await axios.get(`https:/github.com/${githubUsername}`);
+  if(resp.status !== 200) {
+    res.status(404).json({ error: "GitHub user not found" });
+  }
+
+  const userData = githubScraper(githubUsername);
+
 
   res.status(200).json({ message: "Interview started", interviewId: 1 });
 }
